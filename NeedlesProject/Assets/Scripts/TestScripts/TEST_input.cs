4eaf31399231c8784b38a2911db157adc9fbe62a
Jackal0024 <icemaunntenn@gmail.com>
2017-08-31 23:43:00 +0900

    テスト中

---
+++ NeedlesProject/Assets/Scripts/TestScripts/TEST_input.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEST_input : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Vector3 dir = new Vector3(x, y, 0);
+
+        float defeated = Mathf.Min(1.0f, (Mathf.Abs(dir.x) + Mathf.Abs(dir.y)));
+        float len = defeated * 5;
+
+        Debug.DrawRay(transform.localPosition, dir.normalized * len, Color.black);
+        transform.localPosition = Vector3.zero;
+        float angle = Vector3.Dot(Quaternion.AngleAxis(90, Vector3.forward) * transform.forward, dir.normalized);
+        if (dir != Vector3.zero) transform.GetComponent<Rigidbody>().angularVelocity = (Vector3.forward * (angle * 10));
+        transform.transform.localScale = new Vector3(1, 1, len);
+
+    }
+}
