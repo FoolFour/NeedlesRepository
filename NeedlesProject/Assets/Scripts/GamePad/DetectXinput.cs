4874f3bed29862c1a51cf08ca6618c0548204aea
G014A1628\ralsoon <smallkirby423@yahoo.co.jp>
2017-09-08 18:17:35 +0900

    DetectXinputを更新 ゲーム中にゲームパッドが抜き差しされた場合を検知するように変更

--- NeedlesProject/Assets/Scripts/GamePad/DetectXinput.cs
+++ NeedlesProject/Assets/Scripts/GamePad/DetectXinput.cs
@@ -5,9 +5,11 @@ using System.Globalization;
 
 public class DetectXinput : MonoBehaviour
 {
-    [SerializeField]
-    private int useGamePadNumber;
 
+    [SerializeField]
+    private bool   detectOnlyOnce;
+
+    private string useControllerName = "";
 #if UNITY_EDITOR
     [SerializeField]
     private bool showLog;
@@ -21,31 +23,77 @@ public class DetectXinput : MonoBehaviour
 
     private void Awake()
     {
-        Log("コントローラーがXinputかどうか開始");
+        Log("コントローラーのタイプの判別開始");
         var joysticks = Input.GetJoystickNames();
-        if (joysticks.Length >= 1)
-        {
-            Log("使うコントローラーの名前 : " + joysticks[useGamePadNumber]);
 
-            if (IsXinput(joysticks[useGamePadNumber]))
+        bool isDetected = false;
+
+        //1Pのコントローラーが常に配列の0番目にあるとは限らない為
+        foreach (string name in joysticks)
+        {
+            if (name != "")
             {
-                Log("Xinput検知！ Xinput用の設定を適用します");
-                GamePad.SetXinputMode();
-            }
-            else
-            {
-                Log("Xinput以外を検知！ Direct用の設定を適用します");
-                GamePad.SetDirectMode();
+                SetControllerMode(name);
+                useControllerName = name;
+                isDetected = true;
             }
         }
-        else
+
+        if (!isDetected)
         {
-            Log("<color=0x888800>コントローラーの判別失敗 :</color> コントローラーが接続されていません");
+            Log("<color=orange>コントローラーの判別失敗 :</color> コントローラーが接続されていません");
         }
         Log("コントローラの判別処理をを終了します");
     }
 
-    bool IsXinput(string joystickName)
+    private void Start()
+    {
+        if (detectOnlyOnce)
+        {
+            Destroy(this);
+        }
+    }
+
+    private void Update()
+    {
+        var  joysticks  = Input.GetJoystickNames();
+        bool isDetected = false;
+
+        foreach (string name in joysticks)
+        {
+            if (name == "") { continue; }
+
+            isDetected = true;
+            if (name == useControllerName) { break; }
+
+            Log("コントローラーのタイプの判別開始");
+            SetControllerMode(name);
+            useControllerName = name;
+            Log("コントローラの判別処理をを終了します");
+        }
+
+        if (!isDetected)
+        {
+            useControllerName = "";
+        }
+    }
+
+    private void SetControllerMode(string joystickName)
+    {
+        Log("使うコントローラーの名前 : " + joystickName);
+        if (IsXinput(joystickName))
+        {
+            Log("Xinput検知！ Xinput用の設定を適用します");
+            GamePad.SetXinputMode();
+        }
+        else
+        {
+            Log("Xinput以外を検知！ Direct用の設定を適用します");
+            GamePad.SetDirectMode();
+        }
+    }
+
+    private bool IsXinput(string joystickName)
     {
         //ゲームパッドによって[Xinput]の文字が全て小文字の場合や全て大文字の可能性を考慮し
         //大文字と小文字を無視して判定するようにする
