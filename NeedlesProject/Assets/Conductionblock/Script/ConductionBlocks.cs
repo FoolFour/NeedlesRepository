6c9562738497ad9afb2cd946f8602e59c1e06cc1
Saeki <@saeki.com>
2017-09-12 14:27:09 +0900

    電導のプレハブ、スクリプト、マテリアル

---
+++ NeedlesProject/Assets/Conductionblock/Script/ConductionBlocks.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConductionBlocks : MonoBehaviour {
+
+    //電導スイッチのブロックの処理
+
+    //スイッチのon,off
+    public bool blockSwitch = false; 
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //エフェクトなど表示するかも
+        if (blockSwitch == true)
+        {
+
+        }
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        blockSwitch = true;
+    }
+
+
+    void OnCollisionExit(Collision collision)
+    {
+        blockSwitch = false;
+    }
+}
