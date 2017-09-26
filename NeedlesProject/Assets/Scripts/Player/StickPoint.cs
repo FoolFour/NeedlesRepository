fae0e6128a0044dcc9bfc99567867926c129f883
Jackal0024 <icemaunntenn@gmail.com>
2017-09-26 13:38:25 +0900

    ファイル構成変更

--- NeedlesProject/Assets/Scripts/Player/StickPoint.cs
+++ NeedlesProject/Assets/Scripts/Player/StickPoint.cs
@@ -2,17 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Joint : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class StickPoint : MonoBehaviour {
 
     void OnJointBreak(float breakForce)
     {
