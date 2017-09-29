64c4f3cb8fccb410dcf6eaeb57948bd775414481
G014A1628\ralsoon <smallkirby423@yahoo.co.jp>
2017-09-29 17:30:34 +0900

    ステージエディタの更新

---
+++ NeedlesProject/Assets/Editor/StageEditor/Scripts/RichTextFormat.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Format
+{
+    //俺式リッチテキストフォーマット
+    public static class RichText
+    {
+        public static string Success(string text)
+        {
+            return "<color=lime>" + text + "</color>";
+        }
+
+        public static string Failed(string text)
+        {
+            return "<color=orange>" + text + "</color>";
+        }
+
+        public static string Info(string text)
+        {
+            return "<color=blue>" + text + "</color>";
+        }
+    }
+}
