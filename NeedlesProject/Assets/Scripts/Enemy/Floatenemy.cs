d56502b57f73dd96c52315f7761791c1061de01c
Saeki <@saeki.com>
2017-10-24 16:19:03 +0900

    敵の追加 浮遊してる方はほぼ完成

---
+++ NeedlesProject/Assets/Scripts/Enemy/Floatenemy.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floatenemy : BlockBase
+{
+    //移動速度
+    public float movespeed;
+    //移動範囲
+    public float min;
+    public float max;
+    //
+    float movenum;
+
+    //向き
+    public enum LR
+    {
+        LEFT,
+        RIGHT
+    }
+    public LR leftright;
+    void Start()
+    {
+        leftright = LR.RIGHT;
+    }
+
+    void Update()
+    {
+        //はしこっまで行ったら逆に移動
+        if (movenum >= 3)
+        {
+            leftright = LR.RIGHT;
+        }
+        else if(movenum <= -3)
+        {
+            leftright = LR.LEFT;
+        }
+
+        if (leftright==LR.LEFT)
+        {
+            movenum += 0.1f * Time.deltaTime;
+            gameObject.transform.position += transform.right * movespeed * Time.deltaTime;
+        }
+        else
+        {
+            movenum -= 0.1f * Time.deltaTime;
+            gameObject.transform.position -= transform.right * movespeed * Time.deltaTime;
+        }
+
+    }
+
+    public override void StickEnter(GameObject arm)
+    {
+        Debug.Log("敵に当たった");
+        base.StickEnter(arm);
+    }
+}
---
+++ NeedlesProject/Assets/Scripts/Enemy/nomaleemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nomaleemy : BlockBase
+{
+    //移動速度
+    public float movespeed;
+
+    string undername;
+
+    RaycastHit hit;
+
+    public Transform CenterOfBalance;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        gameObject.transform.position += transform.right * movespeed * Time.deltaTime;
+
+        if(Physics.Raycast( 
+            CenterOfBalance.position,   
+            -transform.up,
+            out hit,
+            float.PositiveInfinity))
+        {
+            Quaternion q = Quaternion.FromToRotation(
+                transform.up, hit.normal);
+
+            transform.rotation *= q;
+        }
+    }
+
+    public override void StickEnter(GameObject arm)
+    {
+        Debug.Log("敵に当たった");
+        base.StickEnter(arm);
+    }
+}
