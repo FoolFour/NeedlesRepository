a6bf008f116a927137ad70610cfbbcb5b2132437
Saeki <@saeki.com>
2017-10-27 16:37:37 +0900

    地上を移動する敵

--- NeedlesProject/Assets/Scripts/Enemy/nomaleemy.cs
+++ NeedlesProject/Assets/Scripts/Enemy/nomaleemy.cs
@@ -6,12 +6,28 @@ public class nomaleemy : BlockBase
 {
     //移動速度
     public float movespeed;
-
+    //
     string undername;
-
+    //
+    Ray ray;
+    Ray ray2;
+    //
     RaycastHit hit;
+    //
+    float distance = 1.0f;
+    //
+    bool ishit;
+    bool ishit2;
+    public Vector3 eulerAngles;
 
-    public Transform CenterOfBalance;
+    public enum rotation
+    {
+        REVERSE01,
+        REVERSE02,
+        MOVE,
+    }
+    public rotation rotation_;
+
     void Start()
     {
 
@@ -19,19 +35,66 @@ public class nomaleemy : BlockBase
 
     void Update()
     {
-        gameObject.transform.position += transform.right * movespeed * Time.deltaTime;
+        eulerAngles = gameObject.transform.eulerAngles;
 
-        if(Physics.Raycast( 
-            CenterOfBalance.position,   
-            -transform.up,
-            out hit,
-            float.PositiveInfinity))
+        ray = new Ray(transform.position, new Vector3(1, -1, 0));
+        Debug.DrawRay(ray.origin, ray.direction * distance, Color.red);
+        ishit = Physics.Raycast(ray, out hit, Mathf.Infinity);
+
+        ray2 = new Ray(transform.position, new Vector3(-1, -1, 0));
+        Debug.DrawRay(ray2.origin, ray2.direction * distance, Color.red);
+        ishit2 = Physics.Raycast(ray2, out hit, Mathf.Infinity);
+
+        //Debug.Log(hit.collider.gameObject.name);
+
+        //移動中
+        if (rotation_ == rotation.MOVE)
         {
-            Quaternion q = Quaternion.FromToRotation(
-                transform.up, hit.normal);
-
-            transform.rotation *= q;
+            gameObject.transform.position +=transform.right * movespeed * Time.deltaTime;
         }
+
+        //ブロックがなければ
+        if (ishit == false)
+        {
+            if (eulerAngles.y >= 0)
+            {
+                Debug.Log("yが0の時");
+                rotation_ = rotation.REVERSE01;
+            }
+        }
+
+        if (ishit2 == false)
+        {
+            if (eulerAngles.y >= 180)
+            {
+                Debug.Log("yが180の時");
+                rotation_ = rotation.REVERSE02;
+            }
+        }
+
+        //
+        if (rotation_ == rotation.REVERSE01)
+        {
+            Debug.Log("回転中");
+
+            float angle = Mathf.LerpAngle(0, 180, 10.0f);
+            transform.eulerAngles = new Vector3(0, angle, 0);
+            if (eulerAngles.y == 180)
+            {
+                rotation_ = rotation.MOVE;
+            }
+        }
+
+        if (rotation_ == rotation.REVERSE02)
+        {
+            float angle = Mathf.LerpAngle(0, 0, 10.0f);
+            transform.eulerAngles = new Vector3(0, angle, 0);
+            if (eulerAngles.y == 0)
+            {
+                rotation_ = rotation.MOVE;
+            }
+        }
+
     }
 
     public override void StickEnter(GameObject arm)
