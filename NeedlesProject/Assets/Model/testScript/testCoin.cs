e41c3f62d35f470e5fa4e6692670e790cfde58b4
tunomizu <tumikihurai46@gmail.com>
2017-11-03 04:55:21 +0900

    アルファーステージ制作

---
+++ NeedlesProject/Assets/Model/testScript/testCoin.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testCoin : MonoBehaviour {
+
+    private Transform _child;
+    // Use this for initialization
+    void Start () {
+        _child = transform.FindChild("default");
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        _child.transform.Rotate(new Vector3(0, 5, 0));
+
+    }
+    public void OnTriggerEnter(Collider collision)
+    {
+        Destroy(transform.gameObject);
+        
+    }
+}
