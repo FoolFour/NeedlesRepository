7a3a9614bdde14297f7f84e6ecb14d7dd7bc8262
Saeki <@saeki.com>
2017-11-07 11:33:54 +0900

    浮いている敵の修正

--- NeedlesProject/Assets/Scripts/Enemy/Floatenemy.cs
+++ NeedlesProject/Assets/Scripts/Enemy/Floatenemy.cs
@@ -7,48 +7,127 @@ public class FloatEnemy : BlockBase
     //移動速度
     public float movespeed;
     //移動範囲
-    public float min;
-    public float max;
-    //
+    public float distance;
+    //移動量
     float movenum;
-
     //public LayerMask mask;
-
-    //向き
-    public enum LR
+    //初期位置
+    Vector3 startPosition;
+    //現在位置
+    Vector3 currentposition;
+    
+    public enum Direction
     {
         LEFT,
-        RIGHT
+        RIGHT,
+        UP,
+        DOWN,
+        RETURN
     }
-    public LR leftright;
+
+    public Direction direction_;
     void Start()
     {
-        leftright = LR.RIGHT;
+        startPosition = gameObject.transform.localPosition;
     }
 
     void Update()
     {
-        //はしこっまで行ったら逆に移動
-        if (movenum >= max)
-        {
-            leftright = LR.RIGHT;
-        }
-        else if(movenum <= min)
-        {
-            leftright = LR.LEFT;
-        }
+        //現在地の更新
+        currentposition = gameObject.transform.localPosition;
 
-        if (leftright==LR.LEFT)
+        if (direction_ == Direction.LEFT)
+        {
+            movenum += movespeed * Time.deltaTime;
+            gameObject.transform.position -= transform.right * movespeed * Time.deltaTime;
+            //指定された位置まで行ったら
+            if (distance <= movenum)
+            {
+                direction_ = Direction.RETURN;
+            }
+        }
+        else if (direction_ == Direction.RIGHT)
         {
             movenum += movespeed * Time.deltaTime;
             gameObject.transform.position += transform.right * movespeed * Time.deltaTime;
+            //指定された位置まで行ったら
+            if (distance <= movenum)
+            {
+                direction_ = Direction.RETURN;
+            }
         }
-        else
+        else if (direction_ == Direction.UP)
         {
-            movenum -=movespeed * Time.deltaTime;
-            gameObject.transform.position -= transform.right * movespeed * Time.deltaTime;
+            movenum += movespeed * Time.deltaTime;
+            gameObject.transform.position += transform.up * movespeed * Time.deltaTime;
+            //指定された位置まで行ったら
+            if (distance <= movenum)
+            {
+                direction_ = Direction.RETURN;
+            }
+        }
+        else if (direction_ == Direction.DOWN)
+        {
+            movenum += movespeed * Time.deltaTime;
+            gameObject.transform.position -= transform.up * movespeed * Time.deltaTime;
+            //指定された位置まで行ったら
+            if (distance <= movenum)
+            {
+                direction_ = Direction.RETURN;
+            }
         }
 
+        //逆向きに移動
+        if (direction_ == Direction.RETURN)
+        {
+            //左
+            if (currentposition.x < startPosition.x)
+            {
+                //Debug.Log("左側");
+                movenum -= movespeed * Time.deltaTime;
+                gameObject.transform.position += transform.right * movespeed * Time.deltaTime;
+
+                if(0 >= movenum)
+                {
+                    direction_ = Direction.LEFT;
+                }
+            }
+            //右
+            else if (currentposition.x > startPosition.x)
+            {
+                //Debug.Log("右側");
+                movenum -= movespeed * Time.deltaTime;
+                gameObject.transform.position -= transform.right * movespeed * Time.deltaTime;
+                if (0 >= movenum)
+                {
+                    direction_ = Direction.RIGHT;
+                }
+            }
+            //上
+            else if (currentposition.y > startPosition.y)
+            {
+                Debug.Log("上側");
+                movenum -= movespeed * Time.deltaTime;
+                gameObject.transform.position -= transform.up * movespeed * Time.deltaTime;
+
+                if (0 >= movenum)
+                {
+                    direction_ = Direction.UP;
+                }
+            }
+            //下
+            else if (currentposition.y < startPosition.y)
+            {
+                //Debug.Log("下側");
+                movenum -= movespeed * Time.deltaTime;
+                gameObject.transform.position += transform.up * movespeed * Time.deltaTime;
+                if (0 >= movenum)
+                {
+                    direction_ = Direction.DOWN;
+                }
+            }
+
+        }
     }
 
     public override void StickEnter(GameObject arm)
