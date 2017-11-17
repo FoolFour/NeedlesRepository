94b59d1d8353812626180adac77b68d39e0a5dfc
ralsoon <smallkirby423@yahoo.co.jp>
2017-11-17 15:38:09 +0900

    コインを数えるクラス 経過時間クラス

---
+++ NeedlesProject/Assets/Scripts/GameMain/CoinCounting.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CoinCounting : MonoBehaviour
+{
+    const int defaultCoinNum = 20;
+
+    int startCoinNum;
+
+    public int coinRest
+    {
+        get { return transform.childCount; }
+    }
+
+    public int playerGetCoinNum
+    {
+        get { return defaultCoinNum - coinRest; }
+    }
+
+    ///////////////////
+    // 関数(private) /
+    /////////////////
+
+    private void Awake()
+    {
+        string tmp_name = gameObject.name;
+        Debug.Assert(tmp_name == "Coin", "親の名前を「Coin」にしてください");
+
+        startCoinNum = coinRest;
+    }
+
+    private void CoinNumTest()
+    {
+#if UNITY_EDITOR
+        //これを満たさない場合ビルド時にエラー
+        Debug.Log("コインの枚数チェック");
+
+        if(startCoinNum < defaultCoinNum)
+        {
+            int tmp = defaultCoinNum - startCoinNum;
+            Debug.LogWarning("<color=yellow>警告</color>:コインの枚数が" + tmp + "枚足りません");
+            return;
+        }
+
+        if(startCoinNum > defaultCoinNum)
+        {
+            int tmp = startCoinNum   - defaultCoinNum;
+            Debug.LogWarning("<color=yellow>警告</color>:コインの枚数が" + tmp + "多いです");
+            return;
+        }
+
+        Debug.Log("コインの枚数チェック - <color=lightblue>正常終了</color>");
+#endif
+    }
+
+    private void Update()
+    {
+
+    }
+}
---
+++ NeedlesProject/Assets/Scripts/ScriptCustomEditor/Editor/GameTimerCustom.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(GameTimer))]
+public class GameTimerCustom : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        GameTimer timer = target as GameTimer;
+
+        EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("ステージの経過時間(ポーズを含む)");
+            EditorGUILayout.LabelField(timer.gameTime.ToString("F2"));
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("ステージの経過時間(ポーズを含めない)");
+            EditorGUILayout.LabelField(timer.gameTimeNoPauseTime.ToString("F2"));
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("ポーズの総経過時間");
+            EditorGUILayout.LabelField(timer.pauseTime.ToString("F2"));
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.Space();
+
+        timer.isPause = EditorGUILayout.Toggle("Pause", timer.isPause);
+    }
+}
