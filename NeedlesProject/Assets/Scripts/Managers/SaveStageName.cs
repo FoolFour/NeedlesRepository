51c68c6494808fbe40f43b7c5bf1c2a2bcf19953
ralsoon <smallkirby423@yahoo.co.jp>
2017-11-19 21:50:14 +0900

    リザルトにデータを受け渡しできるように変更

---
+++ NeedlesProject/Assets/Scripts/Managers/SaveStageName.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class SaveStageName : MonoBehaviour
+{
+    private void Awake()
+    {
+        Scene activeScene = SceneManager.GetActiveScene();
+        PlayerPrefs.SetString("Scene", activeScene.name);
+    }
+}
---
+++ NeedlesProject/Assets/Scripts/ResultData.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ResultData : MonoBehaviour
+{
+    private float        time;
+    private int          coin;
+
+    private GameTimer    timer;
+
+    private CoinCounting counting;
+
+    public float clearTime
+    {
+        get { return timer.gameTimeNoPauseTime; }
+    }
+
+    public int   resultGetCoin
+    {
+        get { return counting.playerGetCoinNum; }
+    }
+
+    private IEnumerator Start()
+    {
+        string sceneName = PlayerPrefs.GetString("Scene");
+        Scene stageScene = SceneManager.GetSceneByName(sceneName);
+        GameObject[] obj = stageScene.GetRootGameObjects();
+
+        foreach (var item in obj)
+        {
+            timer = item.GetComponent<GameTimer>();
+            if(timer    != null)
+            {
+                time = timer.gameTimeNoPauseTime;
+            }
+        }
+
+        yield return null;
+
+        foreach (var item in obj)
+        {
+            counting = item.GetComponent<CoinCounting>();
+            if(counting != null)
+            {
+                coin = counting.playerGetCoinNum;
+            }
+        }
+    }
+}
