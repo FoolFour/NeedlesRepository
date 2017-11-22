33afaca69815db23abc3019d368efa99c1c82d3e
Jackal0024 <icemaunntenn@gmail.com>
2017-11-23 00:45:21 +0900

    修正

--- NeedlesProject/Assets/Scripts/BlockAutoTiling.cs
+++ NeedlesProject/Assets/Scripts/BlockAutoTiling.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[ExecuteInEditMode]
+
 public class BlockAutoTiling : MonoBehaviour {
 
     public int x = 0;
@@ -13,12 +13,7 @@ public class BlockAutoTiling : MonoBehaviour {
     // Use this for initialization
     void Start ()
     {
-        renderer = GetComponent<MeshRenderer>();	
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
+        renderer = GetComponent<MeshRenderer>();
         renderer.materials[x].mainTextureScale = new Vector2(transform.localScale.z, transform.localScale.y);
         renderer.materials[y].mainTextureScale = new Vector2(transform.localScale.x, transform.localScale.z);
         renderer.materials[z].mainTextureScale = new Vector2(transform.localScale.x, transform.localScale.y);
