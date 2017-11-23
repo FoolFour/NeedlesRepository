c0f4e50ec95e8ad45044f1ceb0c4d2d7de678e43
Jackal0024 <icemaunntenn@gmail.com>
2017-11-23 17:50:28 +0900

    一方通行のブロック

---
+++ NeedlesProject/Assets/Scripts/Block/OneWayBlock.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneWayBlock : MonoBehaviour
+{
+    GameObject m_parent = null;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_parent = transform.parent.gameObject;
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            m_parent.GetComponent<BoxCollider>().enabled = false;
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            m_parent.GetComponent<BoxCollider>().enabled = true;
+        }
+    }
+}
