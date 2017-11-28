174deef7e319dba6b53fbdfd4223c9f80cd60bfe
Jackal0024 <icemaunntenn@gmail.com>
2017-11-28 19:46:55 +0900

    サウンド関連追加

---
+++ NeedlesProject/Assets/Scripts/Sound/BGMPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMPlayer : MonoBehaviour {
+
+    public string m_bgmKey;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Sound.PlayBgm(m_bgmKey);	
+	}
+}
