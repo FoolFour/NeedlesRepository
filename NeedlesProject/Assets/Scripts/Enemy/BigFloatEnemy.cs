e503de2d979a5d05abcc449cadba90281705d48b
Jackal0024 <icemaunntenn@gmail.com>
2017-12-01 14:32:26 +0900

    大きい敵をついか　

---
+++ NeedlesProject/Assets/Scripts/Enemy/BigFloatEnemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigFloatEnemy : MonoBehaviour
+{
+    private SpawnManager m_SpawnManager;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_SpawnManager = GameObject.Find("GameManager").GetComponent<SpawnManager>();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag.Contains("Player"))
+        {
+            m_SpawnManager.ReSpawn();
+        }
+    }
+
+}
