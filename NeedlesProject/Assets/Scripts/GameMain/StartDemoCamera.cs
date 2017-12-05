a06d73d16864e7dee7ab8a6303e453fa33911d75
Jackal0024 <icemaunntenn@gmail.com>
2017-12-05 13:54:40 +0900

    カメラ更新

--- NeedlesProject/Assets/Scripts/GameMain/StartDemoCamera.cs
+++ NeedlesProject/Assets/Scripts/GameMain/StartDemoCamera.cs
@@ -13,6 +13,14 @@ public class StartDemoCamera : MonoBehaviour {
     private int p1 = 0;
     private int p2 = 1;
 
+    [SerializeField, Tooltip("どれくらい近づくか？")]
+    public float m_playerDistance;
+    [SerializeField, Tooltip("何秒で近付くか")]
+    public float m_playerPanSpeed;
+    private Transform Player;
+
+    private bool CoroutineNow = false;
+
     void Start ()
     {
         Assert.IsFalse(m_MovePoint.Length <= 1, "少な過ぎる!");
@@ -22,19 +30,42 @@ public class StartDemoCamera : MonoBehaviour {
 	
 	void Update ()
     {
+        //プレイヤを探す
+        if (!Player)
+        {
+            Player = GameManagers.Instance.PlayerManager.GetPlayer().transform;
+            return;
+        }
+        if (CoroutineNow) return;
         m_Timer += m_MoveSpeed * Time.deltaTime;
         transform.position = Vector3.Lerp(m_MovePoint[p1].position, m_MovePoint[p2].position, m_Timer);
         if (m_Timer >= 1)
         {
             if (p2 == m_MovePoint.Length - 1)
             {
-                GetComponent<GameCamera.Camera>().enabled = true;
-                Destroy(this);
+                StartCoroutine(CameraPan());
+                CoroutineNow = true;
                 return; //end
             }
             m_Timer = 0;
             p1 = (p1 + 1) % m_MovePoint.Length;
             p2 = (p2 + 1) % m_MovePoint.Length;
         }
+    }
+
+    IEnumerator CameraPan()
+    {
+        var distance = Vector3.Distance(Player.transform.position, transform.position);
+        while (distance > m_playerDistance)
+        {
+            transform.position += transform.forward * m_playerPanSpeed;
+            yield return new WaitForEndOfFrame();
+            distance = Vector3.Distance(Player.transform.position, transform.position);
+        }
+
+        yield return new WaitForSeconds(3f);
+
+        GetComponent<GameCamera.Camera>().enabled = true;
+        Destroy(this);
     }
 }
