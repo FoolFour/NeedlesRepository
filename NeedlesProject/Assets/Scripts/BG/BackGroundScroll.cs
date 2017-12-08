c3b909a283f6d9678350ed162b90cf2832fc385e
Jackal0024 <icemaunntenn@gmail.com>
2017-12-08 22:33:51 +0900

    背景スクロール完成

--- NeedlesProject/Assets/Scripts/BG/BackGroundScroll.cs
+++ NeedlesProject/Assets/Scripts/BG/BackGroundScroll.cs
@@ -7,24 +7,18 @@ public class BackGroundScroll : MonoBehaviour {
     public float m_MinValue;
     private Transform m_Player;
     public float speed = 1;
-    private Vector2 m_FirstPosition;
-
+    public float m_xScrollSpeed = 10;
+    private Vector2 m_FirstPlayerPosition;
     private Vector2 m_CurrentPosition;
 
-    private Transform m_leftImage;
-    private Transform m_rightImage;
+    private float m_playerPrevx = 0;
 
-    private float m_plusPosition = 300;
-    private float m_minusPosition = -300;
 
     // Use this for initialization
     void Start ()
     {
-        m_rightImage = transform.GetChild(0);
-        m_rightImage.localPosition = new Vector3(600, 0, 0);
-        m_leftImage = transform.GetChild(1);
-        m_leftImage.localPosition = new Vector3(-600, 0, 0);
-	}
+
+    }
 
     // Update is called once per frame
     void Update()
@@ -32,16 +26,78 @@ public class BackGroundScroll : MonoBehaviour {
         if (!m_Player)
         {
             m_Player = GameManagers.Instance.PlayerManager.GetPlayer().transform;
-            if(m_Player) m_FirstPosition = m_Player.position;
+            if (m_Player)
+            {
+                m_FirstPlayerPosition = m_Player.position;
+                m_playerPrevx = m_Player.position.x;
+            }
             return;
         }
 
-        m_CurrentPosition.y = (m_FirstPosition.y - m_Player.position.y) * speed;
+        m_CurrentPosition.y = (m_FirstPlayerPosition.y - m_Player.position.y) * speed;
         m_CurrentPosition.y = Mathf.Clamp(m_CurrentPosition.y, m_MinValue, 0);
 
-        m_CurrentPosition.x = (m_FirstPosition.x - m_Player.position.x);
+        float move = m_playerPrevx - m_Player.position.x;
+        m_playerPrevx = m_Player.position.x;
 
-        GetComponent<RectTransform>().localPosition = m_CurrentPosition;
+        {
+            var temp = GetComponent<RectTransform>().localPosition;
+            temp.x += move * m_xScrollSpeed;
+            GetComponent<RectTransform>().localPosition = temp;
+        }
 
+        float halfWidth = 1280 / 2;
+        if(GetComponent<RectTransform>().localPosition.x >= 1280 + halfWidth)
+        {
+            var temp = GetComponent<RectTransform>().localPosition;
+            temp.x = SearchRightEndmost().localPosition.x - 1280;
+            GetComponent<RectTransform>().localPosition = temp;
+        }
+        else if(GetComponent<RectTransform>().localPosition.x <= -1280 - halfWidth)
+        {
+            var temp = GetComponent<RectTransform>().localPosition;
+            temp.x = SearchLeftEndmost().localPosition.x + 1280;
+            GetComponent<RectTransform>().localPosition = temp;
+        }
+
+    }
+
+    /// <summary>
+    /// 兄弟で一番右端を見つける（プラス側）
+    /// </summary>
+    private RectTransform SearchRightEndmost()
+    {
+        float end = 0;
+        RectTransform endmost = null;
+        foreach(var rt in transform.parent.GetComponentsInChildren<RectTransform>())
+        {
+            if (end > rt.localPosition.x)
+            {
+                end = rt.localPosition.x;
+                endmost = rt;
+            }
+        }
+        Debug.Log(endmost.name);
+        return endmost;
+    }
+
+    /// <summary>
+    /// 兄弟で一番左端を見つける（マイナス側）
+    /// </summary>
+    /// <returns></returns>
+    private RectTransform SearchLeftEndmost()
+    {
+        float end = 0;
+        RectTransform endmost = null;
+        foreach (var rt in transform.parent.GetComponentsInChildren<RectTransform>())
+        {
+            if (end < rt.localPosition.x)
+            {
+                end = rt.localPosition.x;
+                endmost = rt;
+            }
+        }
+        Debug.Log(endmost.name);
+        return endmost;
     }
 }
