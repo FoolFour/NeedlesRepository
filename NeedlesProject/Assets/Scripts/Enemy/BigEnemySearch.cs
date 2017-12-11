793cc7fbafbed465ff5cba937a8e909283f3bf72
Jackal0024 <icemaunntenn@gmail.com>
2017-12-11 18:19:08 +0900

    searchシステムを変更

--- NeedlesProject/Assets/Scripts/Enemy/BigEnemySearch.cs
+++ NeedlesProject/Assets/Scripts/Enemy/BigEnemySearch.cs
@@ -1,17 +1,23 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BigEnemySearch : MonoBehaviour
+public class BigEnemySearch : MonoBehaviour, IRespawnMessage
 {
     public BigFloatEnemy m_bigEnemy;
 
-    public void OnTriggerExit(Collider other)
+    public void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
             m_bigEnemy.GetComponent<MoveBlock>().SwitchOn();
-            Destroy(gameObject);
+            GetComponent<RemoveComponent>().SwitchActive(false);
         }
+    }
+
+    public void RespawnInit()
+    {
+        GetComponent<RemoveComponent>().SwitchActive(true);
     }
 }
