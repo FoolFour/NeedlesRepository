4b767ce5c2e67b4407b9115ea4cbdb425c2c322f
Saeki <@saeki.com>
2017-12-12 16:34:18 +0900

    パーティクルもろもろ追加

---
+++ NeedlesProject/Assets/Particle/Script/EfectManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EfectManager : MonoBehaviour
+{
+    public List<ParticleSystem> particles = new List<ParticleSystem>();
+
+    //デバッグ用
+    public bool debugStart;
+
+    // Use this for initialization
+    void Start()
+    {
+        ParticlesStop();
+        debugStart = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //デバッグ用
+        if (debugStart == true)
+        {
+            ParticlesStart();
+            debugStart = false;
+        }
+    }
+
+    //パーティクルスタート
+    void ParticlesStart()
+    {
+        for (int i = 0; i < particles.ToArray().Length; i++)
+        {
+            particles[i].Play();
+        }
+    }
+
+    //パーティクル停止
+    void ParticlesStop()
+    {
+        for (int i = 0; i < particles.ToArray().Length; i++)
+        {
+            particles[i].Stop();
+        }
+    }
+}
---
+++ NeedlesProject/Assets/Particle/Script/ShootingStar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingStar : MonoBehaviour {
+
+    public GameObject gameobj;
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = gameobj.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb.AddForce(-1,0,0,ForceMode.Impulse);
+	}
+
+
+}
