247c93a674356a6eb3ff3666cd13b169a36b0ac7
tunomizu <tumikihurai46@gmail.com>
2017-12-13 00:04:40 +0900

    惑星制作途中経過

---
+++ NeedlesProject/Assets/Model/AnimationScript/WorldRotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldRotation : MonoBehaviour {
+
+    public float RotationSpeed = 5;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(0, RotationSpeed, 0));
+    }
+}
