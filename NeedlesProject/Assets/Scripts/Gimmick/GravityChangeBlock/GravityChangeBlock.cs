25edb13829778a94f633593114cf9ea2b0530dcd
Jackal0024 <icemaunntenn@gmail.com>
2017-12-13 22:28:46 +0900

    重力変化ブロック追加

---
+++ NeedlesProject/Assets/Scripts/Gimmick/GravityChangeBlock/GravityChangeBlock.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityChangeBlock : BlockBase {
+
+    [Tooltip("変化する重力")]
+    public Vector2 m_Gravity;
+
+    public override void StickEnter(GameObject arm)
+    {
+        Physics.gravity = m_Gravity;
+    }
+}
