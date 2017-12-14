d6da3c32234dc02463d4c5528aebbf8a103a8768
ralsoon <smallkirby423@yahoo.co.jp>
2017-12-14 21:17:17 +0900

    ポーズ関係の

---
+++ NeedlesProject/Assets/Scripts/GameMain/PauseUI/ShowTime.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Pause
+{
+    public class ShowTime : MonoBehaviour
+    {
+        PauseUIData data;
+
+        private void Reset()
+        {
+            data = FindObjectOfType<PauseUIData>();
+        }
+
+        private void Awake()
+        {
+            if(data == null)
+            {
+                
+            }
+        }
+
+        private void Start()
+        {
+
+        }
+
+        private void Update()
+        {
+
+        }
+    }
+}
