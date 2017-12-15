d9dbed76346e4119bcfd03b5fa84dc4d0d2d7f0c
Jackal0024 <icemaunntenn@gmail.com>
2017-12-15 16:14:09 +0900

    Moveブロックの機能追加

--- NeedlesProject/Assets/Scripts/Gimmick/MoveBlock/MoveBlock.cs
+++ NeedlesProject/Assets/Scripts/Gimmick/MoveBlock/MoveBlock.cs
@@ -6,11 +6,19 @@ using UnityEngine.Assertions;
 
 public class MoveBlock : MonoBehaviour,IRespawnMessage {
 
+    public enum MoveMode
+    {
+        Lerp,
+        Speed,
+    }
+
     [SerializeField,Tooltip("軌道")]
     public Transform[] m_MovePoint;
     public float m_MoveSpeed;
     [SerializeField, Tooltip("スイッチ使って起動するタイプか？")]
     public bool isSwitchType = false;
+    [SerializeField, Tooltip("移動タイプ")]
+    public MoveMode m_moveMode = MoveMode.Lerp;
     public bool isLoop = true;
 
     private float m_Timer = 0.0f;
@@ -30,16 +38,39 @@ public class MoveBlock : MonoBehaviour,IRespawnMessage {
 	void Update ()
     {
         if (isSwitchMode) return;
+        switch(m_moveMode)
+        {
+            case MoveMode.Lerp: MoveModeLerp();break;
+            case MoveMode.Speed:MoveModeSpeed(); break;
+        }
+	}
+
+    private void MoveModeLerp()
+    {
         m_Timer += m_MoveSpeed * Time.deltaTime;
-        transform.position = Vector3.Lerp(m_MovePoint[p1].position, m_MovePoint[p2].position,m_Timer);
-        if(m_Timer >= 1)
+        transform.position = Vector3.Lerp(m_MovePoint[p1].position, m_MovePoint[p2].position, m_Timer);
+        if (m_Timer >= 1)
         {
             if (!isLoop && p2 == m_MovePoint.Length - 1) return;
             m_Timer = 0;
             p1 = (p1 + 1) % m_MovePoint.Length;
             p2 = (p2 + 1) % m_MovePoint.Length;
         }
-	}
+    }
+
+    private void MoveModeSpeed()
+    {
+        var velocity = (m_MovePoint[p2].position - transform.position).normalized * m_MoveSpeed;
+        var distance = Vector3.Distance(m_MovePoint[p2].position, transform.position);
+        if(velocity.magnitude >= distance)
+        {
+            if (!isLoop && p2 == m_MovePoint.Length - 1) return;
+            transform.position = m_MovePoint[p2].position;
+            p2 = (p2 + 1) % m_MovePoint.Length;
+            return;
+        }
+        transform.position += velocity;
+    }
 
     public void SwitchOn()
     {
