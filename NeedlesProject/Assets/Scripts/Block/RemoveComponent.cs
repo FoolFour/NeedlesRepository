aeb02a875c6c24f9867468fb716a13710003bbf0
Jackal0024 <icemaunntenn@gmail.com>
2017-12-20 11:31:53 +0900

    Player fix

--- NeedlesProject/Assets/Scripts/Block/RemoveComponent.cs
+++ NeedlesProject/Assets/Scripts/Block/RemoveComponent.cs
@@ -7,12 +7,12 @@ public class RemoveComponent : MonoBehaviour {
     public void SwitchActive(bool flag)
     {
         var renderer = GetComponent<MeshRenderer>();
-        var collider = GetComponent<BoxCollider>();
+        var collider = GetComponent<Collider>();
         if (renderer) renderer.enabled = flag;
         if (collider) collider.enabled = flag;
 
         var renderers = GetComponentsInChildren<MeshRenderer>();
-        var colliders = GetComponentsInChildren<BoxCollider>();
+        var colliders = GetComponentsInChildren<Collider>();
         foreach(var rn in renderers) { rn.enabled = flag; }
         foreach (var co in colliders) { co.enabled = flag; }
     }
