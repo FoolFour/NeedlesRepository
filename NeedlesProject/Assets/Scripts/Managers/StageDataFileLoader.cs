a9beab093742615b2b81fb46aa93deb15b11e490
ralsoon <smallkirby423@yahoo.co.jp>
2017-12-22 02:56:44 +0900

    Resultのバグ修正

---
+++ NeedlesProject/Assets/Scripts/Managers/StageDataFileLoader.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using IO = System.IO;
+
+public class StageDataFileLoader : MonoBehaviour
+{
+    public string nowStageData;
+    public string nextStageScene;
+
+    private void Awake()
+    {
+        using (var fs = new IO.FileStream(nowStageData, IO.FileMode.Open))
+        {
+            using (var br = new IO.BinaryReader(fs))
+            {
+                string stage_name = br.ReadString();
+                string scene_name = br.ReadString();
+                float  border1    = br.ReadSingle();
+                float  border2    = br.ReadSingle();
+
+                PlayerPrefs.SetString(PrefsDataName.StageName, stage_name);
+                PlayerPrefs.SetString(PrefsDataName.Scene,     scene_name);
+                PlayerPrefs.SetFloat (PrefsDataName.Border1,   border1);
+                PlayerPrefs.SetFloat (PrefsDataName.Border2,   border2);
+
+                br.Close();
+                fs.Close();
+            }
+        }
+        
+        PlayerPrefs.SetString(PrefsDataName.NextSene, nextStageScene);
+    }
+}
--- NeedlesProject/Assets/Scripts/WorldSelect/StageSubmitToSceneChange.cs
+++ NeedlesProject/Assets/Scripts/WorldSelect/StageSubmitToSceneChange.cs
@@ -25,22 +25,7 @@ public class StageSubmitToSceneChange : MonoBehaviour
             //情報を送る
             var stage_info = info.GetSelectStageInfo();
 
-            var stage_name = stage_info.stageName;
-            Debug.Log("ステージ名:"     + stage_name);
-            PlayerPrefs.SetString(PrefsDataName.StageName, stage_name);
-
             var scene_name = stage_info.sceneName;
-            Debug.Log("シーン名:"       + scene_name);
-            PlayerPrefs.SetString(PrefsDataName.Scene,     scene_name);
-
-            var border1   = stage_info.border1;
-            Debug.Log("ボーダータイム1:" + border1);
-            PlayerPrefs.SetFloat(PrefsDataName.Border1,    border1);
-
-            var border2   = stage_info.border2;
-            Debug.Log("ボーダータイム2:" + border2);
-            PlayerPrefs.SetFloat(PrefsDataName.Border2,    border2);
-
             sceneChange.SceneChange(scene_name);
         }
     }
