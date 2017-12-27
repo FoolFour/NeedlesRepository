664c667ce82486f94f916f6f6c7e13c3af0e1a3e
Jackal0024 <icemaunntenn@gmail.com>
2017-12-27 23:40:59 +0900

    重力変化系修正

--- NeedlesProject/Assets/Scripts/Gimmick/GravityChangeArea/GravityChangeArea.cs
+++ NeedlesProject/Assets/Scripts/Gimmick/GravityChangeArea/GravityChangeArea.cs
@@ -1,26 +1,51 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GravityChangeArea : MonoBehaviour
+public class GravityChangeArea : MonoBehaviour, IRespawnMessage
 {
     public Vector3 m_nextGravity = new Vector3(0, 9.81f, 0);
     private Vector3 m_prevGravity;
+    private bool isInside; //プレイヤーが中にいることを証明する
+
+    public void Start()
+    {
+        m_prevGravity = Physics.gravity;
+    }
 
     public void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            isInside = true;
             m_prevGravity = Physics.gravity;
             Physics.gravity = m_nextGravity;
         }
     }
 
-    public void OnTriggerExit(Collider other)
+    public void Update()
     {
-        if (other.CompareTag("Player"))
+        if (!isInside) return;
+        var colliders = Physics.OverlapBox(transform.position, transform.localScale / 2, transform.rotation);
+        foreach (var col in colliders)
         {
-            Physics.gravity = m_prevGravity;
+            if (col.CompareTag("Player")) return;
         }
+        Physics.gravity = m_prevGravity;
+        isInside = false;
+    }
+
+    //public void OnTriggerExit(Collider other)
+    //{
+    //    if (other.CompareTag("Player"))
+    //    {
+    //        Physics.gravity = m_prevGravity;
+    //    }
+   // }
+
+    public void RespawnInit()
+    {
+        Physics.gravity = m_prevGravity;
     }
 }
--- NeedlesProject/Assets/Scripts/Gimmick/GravityChangeBlock/GravityChangeBlock.cs
+++ NeedlesProject/Assets/Scripts/Gimmick/GravityChangeBlock/GravityChangeBlock.cs
@@ -1,14 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GravityChangeBlock : BlockBase {
+public class GravityChangeBlock : BlockBase, IRespawnMessage
+{
 
     [Tooltip("変化する重力")]
     public Vector2 m_Gravity;
 
+    private Vector3 m_NormalGravity;
+
+    public void Start()
+    {
+        m_NormalGravity = Physics.gravity;
+    }
+
+    public void RespawnInit()
+    {
+        Physics.gravity = m_Gravity;
+    }
+
     public override void StickEnter(GameObject arm)
     {
+        m_NormalGravity = Physics.gravity;
         Physics.gravity = m_Gravity;
     }
 }
