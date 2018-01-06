7bef2ec300935596ba30c3ae25fac17889ac6028
Jackal0024 <icemaunntenn@gmail.com>
2018-01-06 20:32:17 +0900

    ジャンプ台アニメーション追加

--- NeedlesProject/Assets/Scripts/Gimmick/JumpBlock/JumpArea.cs
+++ NeedlesProject/Assets/Scripts/Gimmick/JumpBlock/JumpArea.cs
@@ -6,11 +6,21 @@ public class JumpArea : MonoBehaviour
 {
     [Tooltip("ジャンプ力")]
     public float m_Power = 10;
+
+    Animator m_Animator;
+
+    public void Start()
+    {
+        m_Animator = GetComponentInParent<Animator>();
+    }
+
     public void OnTriggerExit(Collider other)
     {
-        if(other.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
+            m_Animator.SetTrigger("JumpTr");
             other.GetComponent<Rigidbody>().AddForce(transform.up * m_Power, ForceMode.VelocityChange);
+
         }
     }
 }
