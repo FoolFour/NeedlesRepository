3e930e12e57397606968c3e4a7a7db501901340e
Jackal0024 <icemaunntenn@gmail.com>
2018-01-06 22:10:46 +0900

    バンパーアニメーション

--- NeedlesProject/Assets/Scripts/Gimmick/Bumper/Bumper.cs
+++ NeedlesProject/Assets/Scripts/Gimmick/Bumper/Bumper.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class Bumper : MonoBehaviour
 {
     public float m_Power = 30;
+
+    Animator m_Animator;
+
+    public void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+    }
+
     public void OnCollisionExit(Collision collision)
     {
-        if(collision.collider.CompareTag("Player"))
+        if (collision.collider.CompareTag("Player"))
         {
+            m_Animator.SetTrigger("BumperOnTr");
             var dir = collision.transform.position - transform.position;
             collision.gameObject.GetComponent<Rigidbody>().AddForce(dir.normalized * m_Power, ForceMode.VelocityChange);
         }
