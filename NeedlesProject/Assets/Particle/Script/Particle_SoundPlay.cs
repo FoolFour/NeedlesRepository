b442b54d8f27be8eb825a860249fd25eada0f9bd
Saeki <@saeki.com>
2018-01-12 15:00:19 +0900

    パーティクルの修正 水しぶき（パ－ティクル・ＳＥ）追加

--- NeedlesProject/Assets/Particle/Script/Particle_SoundPlay.cs
+++ NeedlesProject/Assets/Particle/Script/Particle_SoundPlay.cs
@@ -4,36 +4,50 @@ using UnityEngine;
 
 public class Particle_SoundPlay : MonoBehaviour {
 
-    public AudioClip Bubble_Break;
-    public AudioClip Bubble_Long;
-    public AudioClip Bubble_Heavy_Short;   
-    public AudioClip Bubble_Middle_Short;
+    //SE変数
+    public AudioClip SE_Bubble_Break;
+    public AudioClip SE_Bubble_Long;
+    public AudioClip SE_Bubble_Heavy_Short;   
+    public AudioClip SE_Bubble_Middle_Short;
+    public AudioClip SE_Waterdrop;
 
+    //ランダム
     public int rand;
+
+    //泡を使う場合のフラグ
+    public bool bubbleflag;
+    public bool waterdorop;
+
 
     // Use this for initialization
     void Start () {
-       
-     
+        if (waterdorop == true)
+        {
+            GetComponent<AudioSource>().PlayOneShot(SE_Waterdrop);
+            //waterdorop = false;
+        }
     }
 	
 	// Update is called once per frame
 	void Update () {
         rand = Random.Range(1, 100);
 
-        if (rand%20==0)
+        if (bubbleflag == true)
         {
-            GetComponent<AudioSource>().PlayOneShot(Bubble_Break);
-        }
+            if (rand % 20 == 0)
+            {
+                GetComponent<AudioSource>().PlayOneShot(SE_Bubble_Break);
+            }
 
-        if (rand % 15 == 0)
-        {
-            GetComponent<AudioSource>().PlayOneShot(Bubble_Heavy_Short);
-        }
+            if (rand % 15 == 0)
+            {
+                GetComponent<AudioSource>().PlayOneShot(SE_Bubble_Heavy_Short);
+            }
 
-        if (rand % 10 == 0)
-        {
-            GetComponent<AudioSource>().PlayOneShot(Bubble_Middle_Short);
+            if (rand % 10 == 0)
+            {
+                GetComponent<AudioSource>().PlayOneShot(SE_Bubble_Middle_Short);
+            }
         }
     }
 }
