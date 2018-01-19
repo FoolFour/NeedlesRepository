cba6c14f7a1effe5f00d58ef8dcb5b7c41e04d23
ralsoon <smallkirby423@yahoo.co.jp>
2018-01-19 12:02:29 +0900

    マテリアルを元に戻すスクリプト

---
+++ NeedlesProject/Assets/Editor/MaterialDefaultSetter.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using IO = System.IO;
+
+[InitializeOnLoad]
+public static class MaterialDefaultSetter
+{
+    public  static Material[] material;
+    private static Material[] originalMaterial;
+
+    static MaterialDefaultSetter()
+    {
+        material         = new Material[0];
+        originalMaterial = new Material[0];
+
+        string path = "./Assets/Editor/mateial";
+        if(!IO.File.Exists(path)) { return; }
+
+        using (var rs = new IO.StreamReader(path))
+        {
+            string str = rs.ReadLine();
+            int len = int.Parse(str);
+            
+            Array.Resize(ref material, len);
+
+            for(int i = 0; i < len; i++)
+            {
+                str = rs.ReadLine();
+                material[i] = AssetDatabase.LoadAssetAtPath<Material>(str);
+            }
+        }
+
+        EditorApplication.playmodeStateChanged += OnPlayModeStateChanged;
+    }
+
+    private static void OnPlayModeStateChanged()
+    {
+        if(!EditorApplication.isPlaying &&
+            EditorApplication.isPlayingOrWillChangePlaymode)
+        {
+            originalMaterial = new Material[material.Length];
+            for (int i = 0; i < material.Length; i++)
+            {
+                originalMaterial[i] = new Material(material[i]);
+            }
+        }
+
+        //Editorの再生が終わった時にデフォルトに戻す
+        if( EditorApplication.isPlaying && 
+           !EditorApplication.isPlayingOrWillChangePlaymode)
+        {
+            for (int i = 0; i < originalMaterial.Length; i++)
+            {
+                material[i] = originalMaterial[i];
+            }
+        }
+    }
+}
