388c6ad3eb582f0c9ed227e69cd7183e1c7a6e4d
ralsoon <smallkirby423@yahoo.co.jp>
2018-01-23 15:15:33 +0900

    不要なフォントの削除 フォントを追加

---
+++ NeedlesProject/Assets/Scripts/Utility/FadeRepeat.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FadeRepeat : MonoBehaviour
+{
+    Graphic graphic;
+
+    private void Awake()
+    {
+        graphic = GetComponent<Graphic>();
+    }
+
+    private void Start()
+    {
+
+    }
+
+    //private IEnumerator FadeIn()
+    //{
+
+    //}
+
+    //private IEnumerator FadeOut()
+    //{
+
+    //}
+}
