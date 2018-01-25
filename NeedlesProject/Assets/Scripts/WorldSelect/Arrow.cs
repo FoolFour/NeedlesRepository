f2bde09cca8e4374766ccedc7b6512e3095289e5
ralsoon <smallkirby423@yahoo.co.jp>
2018-01-25 18:11:02 +0900

    ワールド選択に矢印追加

---
+++ NeedlesProject/Assets/Scripts/WorldSelect/Arrow.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Arrow : MonoBehaviour
+{
+    private enum Direction
+    {
+        Left,
+        Right
+    }
+
+    [SerializeField]
+    Direction direction;
+
+    [SerializeField]
+    StageBasicInfoManager info;
+
+    Image image;
+
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+    }
+
+    private void Update()
+    {
+        if(direction == Direction.Left)
+        {
+            image.enabled = !info.IsFirstWorld;
+        }
+
+        if(direction == Direction.Right)
+        {
+            image.enabled = !info.IsLastWorld;
+        }
+    }
+}
