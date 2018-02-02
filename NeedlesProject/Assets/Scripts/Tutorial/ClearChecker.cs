212ce910080d502c004de4be75de17c658c16f10
Jackal0024 <icemaunntenn@gmail.com>
2018-02-02 16:20:13 +0900

    tutorial用クリアflag追加

---
+++ NeedlesProject/Assets/Scripts/Tutorial/ClearChecker.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearChecker : MonoBehaviour {
+
+    public IConditions m_Conditions;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(m_Conditions.IsClear())
+        {
+            Debug.Log("シーンChange");
+        }	
+	}
+}
---
+++ NeedlesProject/Assets/Scripts/Tutorial/CoinClearConditions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinClearConditions : IConditions {
+
+	// Use this for initialization
+	void Start ()
+    {
+        isClear = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(transform.childCount == 0)
+        {
+            isClear = true;
+            Debug.Log("クリア");
+        }	
+	}
+}
