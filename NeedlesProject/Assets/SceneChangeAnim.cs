64b899779fdaa5f7e2394ac8a01460d114253325
ralsoon <smallkirby423@yahoo.co.jp>
2018-02-06 14:08:12 +0900

    リザルト遷移修正

--- NeedlesProject/Assets/SceneChangeAnim.cs
+++ NeedlesProject/Assets/SceneChangeAnim.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SceneChange : StateMachineBehaviour
+public class SceneChangeAnim : StateMachineBehaviour
 {
     //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
