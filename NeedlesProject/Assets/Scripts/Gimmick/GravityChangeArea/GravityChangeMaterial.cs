1d711aab515c77b6aeba40667519cbdce5ebc09f
ralsoon <smallkirby423@yahoo.co.jp>
2018-02-09 00:05:35 +0900

    水のシェーダー導入 これ以上の表現はちょっと技術不足で無理っす

---
+++ NeedlesProject/Assets/Scripts/Gimmick/GravityChangeArea/GravityChangeMaterial.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GravityChangeMaterial : MonoBehaviour
+{
+    static readonly int front = 2;
+    static readonly int top   = 0;
+    static readonly int left  = 1;
+
+    private void Start()
+    {
+        Vector3 scale = transform.localScale;
+        var mats = GetComponent<Renderer>().materials;
+
+        mats[front].SetFloat("_NWidth" , scale.x/2.5f);
+        mats[front].SetFloat("_NHeight", scale.y/2.5f);
+        mats[front].SetFloat("_NOffset", Random.Range(-100.0f, 100.0f));
+
+        mats[  top].SetFloat("_NWidth" , scale.x/2.5f);
+        mats[  top].SetFloat("_NHeight", scale.z/2.5f);
+        mats[  top].SetFloat("_NOffset", Random.Range(-100.0f, 100.0f));
+
+        mats[ left].SetFloat("_NWidth" , scale.z/2.5f);
+        mats[ left].SetFloat("_NHeight", scale.y/2.5f);
+        mats[ left].SetFloat("_NOffset", Random.Range(-100.0f, 100.0f));
+    }
+}
