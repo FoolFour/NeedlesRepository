7b4c5236acb423bc6344f4ee2fe7bebd3a5263bd
Jackal0024 <icemaunntenn@gmail.com>
2018-02-09 10:50:50 +0900

    coin修正

--- NeedlesProject/Assets/Scripts/Tutorial/TutorialCoin.cs
+++ NeedlesProject/Assets/Scripts/Tutorial/TutorialCoin.cs
@@ -1,19 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TutorialCoin : BlockBase
+public class TutorialCoin : BlockBase,IRespawnMessage
 {
     [SerializeField, Tooltip("プレイヤー本体と当たる")]
     public bool CheckPlayer;
     [SerializeField, Tooltip("アームと当たる")]
     public bool CheckArm;
 
+    private RemoveComponent m_RCom;
+    private bool isDead = false;
+
+    public void Start()
+    {
+        m_RCom = GetComponent<RemoveComponent>();
+        isDead = false;
+    }
+
     public override void StickEnter(GameObject arm)
     {
         if (CheckArm)
         {
-            Destroy(gameObject);
+            m_RCom.SwitchActive(false);
+            isDead = true;
         }
         base.StickEnter(arm);
     }
@@ -22,11 +33,30 @@ public class TutorialCoin : BlockBase
     {
         if (CheckPlayer)
         {
-            if (other.CompareTag("Player")) Destroy(gameObject);
+            if (other.CompareTag("Player"))
+            {
+                isDead = true;
+                m_RCom.SwitchActive(false);
+            }
         }
         if (CheckArm)
         {
-            if (other.CompareTag("PlayerArm")) Destroy(gameObject);
+            if (other.CompareTag("PlayerArm"))
+            {
+                isDead = true;
+                m_RCom.SwitchActive(false);
+            }
         }
+    }
+
+    public bool IsDead()
+    {
+        return isDead;
+    }
+
+    public void RespawnInit()
+    {
+        m_RCom.SwitchActive(true);
+        isDead = false;
     }
 }
