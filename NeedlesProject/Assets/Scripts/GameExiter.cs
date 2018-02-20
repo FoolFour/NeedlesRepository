d3f48d105df4f7a10b8df0b00dcd242a0d8b1d69
ralsoon <smallkirby423@yahoo.co.jp>
2018-02-20 11:31:22 +0900

    StartとBackで強制終了するように設定

---
+++ NeedlesProject/Assets/Scripts/GameExiter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class GameExiter : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Update()
+    {
+        if(GamePad.IsHoldBackAndStart())
+        {
+            Debug.Log("ゲームを終了");
+            Application.Quit();
+        }
+    }
+}
