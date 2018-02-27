9ab622f86cef3c5e98915be28cf5883830d8e286
ralsoon <smallkirby423@yahoo.co.jp>
2018-02-27 15:59:32 +0900

    細かい修正

---
+++ NeedlesProject/Assets/Scripts/Utility/OnSubmitPlaySE.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+
+public class OnSubmitPlaySE : MonoBehaviour, ISubmitHandler
+{
+    [SerializeField]
+    string seName;
+
+    public void OnSubmit(BaseEventData eventData)
+    {
+        Sound.PlaySe(seName);
+    }
+}
